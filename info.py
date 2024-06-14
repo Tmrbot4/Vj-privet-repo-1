647d8494ece22660bdd86376ff7b98e75e672b89
Tmrbot4 <166976733+Tmrbot4@users.noreply.github.com>
2024-06-14 13:54:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '23171051'))
 API_HASH = environ.get('API_HASH', '10331d5d712364f57ffdd23417f4513c')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6463623908:AAGhwkWQvdxTo7h2Rb5cV1qvgbVI5MkD1J4")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7412383356:AAH-Gl2Vbcj2mg4LX6MylZJVT-EmH6ldyM0")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
